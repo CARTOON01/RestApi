49c9013c902211f533b5d1b49433c6046a33a2fa
CARTOON <brianngugi224@gmail.com>
2024-03-08 12:19:51 +0300

    Update on user deletion

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,26 @@ import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from 'db/users';
 
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserid = get(req, 'identity._id') as string;
+
+        if (!currentUserid) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserid.toString() !== id.toString()) {
+            return res.sendStatus(403);
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400); 
+    }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['CARTOON-AUTH'];
